ea492a1799c14cfb58f510c295625e4b85b456d6
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-11-29 09:36:38 -0300

    Create Financial Fetcher

---
+++ src/financial.ts
@@ -0,0 +1,106 @@
+const fetch = require("node-fetch");
+
+interface FinancialInformation {
+  usdToBrlRate: number;
+  bitcoinValueInBrl: number;
+  microsoftValueInBrl: number;
+  appleValueInBrl: number;
+}
+
+export async function getLatestFinancialInformation(): Promise<FinancialInformation> {
+  const responses = await Promise.all([
+    getUsdToBrlRate(),
+    getBitcoinValue(),
+    getMicrosoftValue(),
+    getAppleValue(),
+  ]);
+
+  const usdToBrlRate = responses[0];
+  const bitcoinValueInBrl = responses[1] * usdToBrlRate;
+  const microsoftValueInBrl = responses[2] * usdToBrlRate;
+  const appleValueInBrl = responses[3] * usdToBrlRate;
+
+  return {
+    usdToBrlRate,
+    bitcoinValueInBrl,
+    microsoftValueInBrl,
+    appleValueInBrl,
+  };
+}
+
+async function getUsdToBrlRate(): Promise<number> {
+  const url =
+    "https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=usd&to=BRL&amount=1";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cdd4750d8amshc864cb9fa7b9e5bp11ffa2jsn3bc2042f9b45",
+      "X-RapidAPI-Host": "currency-converter5.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data: any = await res.json();
+
+  return parseFloat(data.rates.BRL.rate);
+}
+
+async function getBitcoinValue(): Promise<number> {
+  const url =
+    "https://alpha-vantage.p.rapidapi.com/query?from_currency=BTC&function=CURRENCY_EXCHANGE_RATE&to_currency=USD";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cdd4750d8amshc864cb9fa7b9e5bp11ffa2jsn3bc2042f9b45",
+      "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data: any = await response.json();
+
+  return parseFloat(
+    data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+  );
+}
+
+async function getMicrosoftValue(): Promise<number> {
+  const url =
+    "https://alpha-vantage.p.rapidapi.com/query?interval=1min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cdd4750d8amshc864cb9fa7b9e5bp11ffa2jsn3bc2042f9b45",
+      "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  const values = data["Time Series (1min)"];
+  const lastData = Object.keys(values).pop();
+
+  return parseFloat(values[lastData as string]["4. close"]);
+}
+
+async function getAppleValue(): Promise<number> {
+  const url =
+    "https://alpha-vantage.p.rapidapi.com/query?interval=1min&function=TIME_SERIES_INTRADAY&symbol=AAPL&datatype=json&output_size=compact";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cdd4750d8amshc864cb9fa7b9e5bp11ffa2jsn3bc2042f9b45",
+      "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  const values = data["Time Series (1min)"];
+  const lastData = Object.keys(values).pop();
+
+  return parseFloat(values[lastData as string]["4. close"]);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-const name: string = "Raphael Collin";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { getLatestFinancialInformation } from "./financial";
 
-console.log('Hello, my name is', name);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+setTimeout(() => {
+  getLatestFinancialInformation()
+    .then((financialInfo) => {
+      console.log(
+        "New financial information available: ",
+        JSON.stringify(financialInfo)
+      );
+      io.send("newFinancialInformation", financialInfo);
+    })
+    .catch((err) =>
+      console.error("Error when fetching financial information:", err)
+    );
+}, 1000);
+
+app.get("/health-check", (req, res) => {
+  res.send({ status: "OK" });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
